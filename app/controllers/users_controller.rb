4d5f912dd6910fb55262a75f26d344a4c47fa6e0
togo <kuraisut@gmail.com>
2023-12-10 00:39:19 +0900

    12/9 ドロップメニュー、アカウント情報編集、画像編集以外のdevise終了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    ensure_correct_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    ensure_correct_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    ensure_correct_user
+    if @user.update(params.require(:user).permit(:name, :introduction, :image_user))
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def ensure_correct_user
+      unless @user == current_user
+        redirect_to user_path(current_user)
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

355af8c47123423d4eadd21c0e1577894b0f747e
togo <kuraisut@gmail.com>
2023-12-14 12:57:59 +0900

    reservationsのモデルファイルの不要部分削除

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,8 @@
 class Reservation < ApplicationRecord
-  has_one_attached :image_room
   belongs_to :user
   belongs_to :room
+
+  
 
   def day_count
     (self.check_out - self.check_in).to_i / 86400 
